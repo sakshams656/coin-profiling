b5c8e7f0aa5cbe48eb0b4787c173390412173072
sakshams656 <saksham.s@zebpay.com>
2025-04-07 11:37:43 +0530

    interfaces removed from hook

--- hooks/useWebsocket.ts
+++ hooks/useWebsocket.ts
@@ -1,23 +1,5 @@
 import { useEffect, useState } from "react";
 
-interface WebSocketData {
-  price: string;
-  change: string;
-  isPositive: boolean;
-}
-
-interface WebSocketMessage {
-  type: string;
-  data: {
-    [key: string]: {
-      BuyRate: number;
-      SellRate: number;
-      LTP: number;
-      PercentChange: string;
-    };
-  };
-}
-
 const useWebSocket = (coinSymbol: string): WebSocketData => {
   const [wsData, setWsData] = useState<WebSocketData>({
     price: "₹0.00",
